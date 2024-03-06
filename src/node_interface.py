f0f8f2d9836f115924bc7f3adf93f56e49d2f38c
Pter <longpp17@163.com>
2024-03-07 00:31:55 +0100

    added option to turn off microphone

--- src/node_interface.py
+++ src/node_interface.py
@@ -16,6 +16,8 @@ RATE = 44100
 CHUNK = 1024
 stream = None
 
+stream_microphone = True
+
 async def get_device_index(p: pyaudio.PyAudio):
     devices = p.get_device_count()
     input_devices, output_devices = {}, {}
@@ -30,27 +32,36 @@ async def get_device_index(p: pyaudio.PyAudio):
 
 
 async def get_stream(p: pyaudio.PyAudio, input_device_index: int, output_device_index: int):
-    local_stream = p.open(format=FORMAT,
-                          channels=CHANNELS,
-                          rate=RATE,
-                          input=True,
-                          output=True,
-                          input_device_index=input_device_index,
-                          output_device_index=output_device_index,
-                          frames_per_buffer=CHUNK)
+    if not stream_microphone:
+        local_stream = p.open(format=FORMAT,
+                              channels=CHANNELS,
+                              rate=RATE,
+                              output=True,
+                              output_device_index=output_device_index,
+                              frames_per_buffer=CHUNK)
+    else:
+        local_stream = p.open(format=FORMAT,
+                              channels=CHANNELS,
+                              rate=RATE,
+                              input=True,
+                              output=True,
+                              input_device_index=input_device_index,
+                              output_device_index=output_device_index,
+                              frames_per_buffer=CHUNK)
     return local_stream
 
 
 async def broadcast(local_stream: pyaudio.Stream):
-    while True:
-        data = local_stream.read(CHUNK, exception_on_overflow=False)
-        await sio.emit('audio-buffer', data)
-        await asyncio.sleep(0)
+    if stream_microphone:
+        while True:
+            data = local_stream.read(CHUNK, exception_on_overflow=False)
+            await sio.emit('audio-buffer', data)
+            await asyncio.sleep(0)
 
 
 @sio.on('audio-buffer')
 async def on_audio(data):
-    print("Received audio data from the server")
+    print("Received audio buffer from the server: ", data)
     if stream is not None:
         stream.write(data)
 
@@ -60,7 +71,12 @@ async def main():
 
     print("Available input devices: ")
     print(input_devices)
-    input_device_index = int(await ainput("Enter the index of the device you want to use: "))
+    input_device_index = int(await ainput("Enter the index of the device you want to use, type -1 to listen only: "))
+    if input_device_index == -1:
+        global stream_microphone
+        stream_microphone = False
+        input_device_index = 0
+
 
     print("Available output devices: ")
     print(output_devices)
@@ -68,7 +84,7 @@ async def main():
 
     await sio.connect('http://localhost:3000')
     print('Connected to the server with SID:', sio.sid)
-    await sio.emit('hello', 'Hello from the client')
+
     print("Entering Bootstrap Link, Input 'done' to complete")
     links = []
     while True:
